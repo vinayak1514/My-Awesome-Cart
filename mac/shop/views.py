2cdb8fe88f87970fb16dd0183c00a6b67656a90d
vinayak1514 <75493987+vinayak1514@users.noreply.github.com>
2024-04-13 22:00:45 +0530

    adding mac gile

---
+++ mac/shop/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+def index(request):
+    return render(request,'shop/index.html')
+def about(request):
+    return render(request,'shop/about.html')
+def contact(request):
+    return HttpResponse("We are at contact")
+def tracker(request):
+    return HttpResponse("We are at tracker")
+def search(request):
+    return HttpResponse("We are at search")
+def prodview(request):
+    return HttpResponse("We are at prodview")
+def checkout(request):
+    return HttpResponse("We are at checkout")
